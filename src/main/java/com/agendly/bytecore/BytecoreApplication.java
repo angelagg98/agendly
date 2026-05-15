091c320d84bf76109545e9318a97018e3098b9a9
Alejandro Torres <torresgiraldoalejandro960@gmail.com>
2026-05-14 22:31:21 -0500

    iniciando el proyecto

---
+++ src/main/java/com/agendly/bytecore/BytecoreApplication.java
@@ -0,0 +1,13 @@
+package com.agendly.bytecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytecoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytecoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendly/bytecore/BytecoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendly.bytecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytecoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
